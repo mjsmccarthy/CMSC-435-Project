adf1313d70f3b7ea2a909ed57b203b61a70ed365
Matthew McCarthy <mjsmccarthy@gmail.com>
2019-10-31 10:08:03 -0400

    Removed pandas-ml package because it was calculating statistical values incorrectly

--- features.py
+++ features.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from PyBioMed.PyProtein import AAComposition
 from PyBioMed.PyProtein import CTD
-from pandas_ml import ConfusionMatrix
 
 '''Generates features for the training dataset'''
 def generate_features(df):
@@ -29,17 +28,8 @@ def generate_features(df):
     # Send dataframe to csv
     res_df.to_csv('generated_features.csv', index=False)
 
-'''Generates a confusion matrix from predicted values for a given model and outputs statistics.'''
-def generate_cm_stats(df):
-    y_true = list(df['Label'])
-    y_pred = list(df['prediction(Label)'])
-    confusion_matrix = ConfusionMatrix(y_true, y_pred)
-    confusion_matrix.print_stats()
-
 if __name__ == '__main__':
     # Import datasets
     df_features = pd.read_csv("training_dataset.csv", names=['Sequence', 'Label'])
-    df_predicted = pd.read_csv("predicted_features.csv")
-
     generate_features(df_features)
-    generate_cm_stats(df_predicted)
+    
